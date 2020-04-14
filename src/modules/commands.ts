5f812ede6cda32d2ddc05c9c5b4da2a10d5cd3bb
Austin Howlett   url = git@bitbucket.org:unraveldevops/unravel-profile-service.git <austinhowletttech@gmail.com>
2020-04-13 23:30:06 -0400

    feat: major change Now reads all commands under /commands dir and dynamically creates new instances of the exported classes to populate the bots list of commands

--- src/modules/commands.ts
+++ src/modules/commands.ts
@@ -1,12 +1,19 @@
-import {CommandsEnum} from './../common/CommandsEnum'
-import config from '../config'
-import {HeadsOrTailsCommand} from './../commands/HeadsOrTails'
+import fs from 'fs'
 import {CommandFactory} from '../common/CommandFactory'
-import {RoundRobinCommand} from './../commands/RoundRobin'
+import {InstanceLoader} from '../common/InstanceLoader'
+import {BaseCommandInterface} from '../shared/Base'
 
 export const bootstrapCommands = async () => {
-  CommandFactory.populateCommandsCollection([
-    {create: () => new RoundRobinCommand()},
-    {create: () => new HeadsOrTailsCommand()},
-  ])
+  const commandFiles = fs
+    .readdirSync(__dirname + '/../commands')
+    .filter(file => file.endsWith('.ts'))
+
+  commandFiles.forEach((file, index) => {
+    import(`../commands/${file}`).then((obj: BaseCommandInterface) => {
+      const instance: BaseCommandInterface = InstanceLoader.getInstance<
+        typeof obj
+      >(obj)
+      CommandFactory.populateCommandsCollection([instance])
+    })
+  })
 }
